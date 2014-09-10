9b060d1392d2f3b85b76b976106d04958f146265
FujiiNoritsugu <quercus491@gmail.com>
2014-09-11 06:12:13 +0900

    monitor
    20140911

---
+++ monitor.js
@@ -0,0 +1,38 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+
+var fs = require('fs');
+var html = fs.readFileSync('monitor.html', 'utf8');
+
+app.listen(8124);
+
+var target_socket;
+io.sockets.on('connection', function(socket){
+	console.log("connection");
+	socket.on('message', function(data){
+		console.log("message = "+data);
+		target_socket = this;
+	});
+	
+	if(target_socket != null){
+		target_socket.send("send data");
+		console.log("send data");
+	}
+	setInterval(tick, 1000);
+});
+
+function handler(req, res){
+ res.setHeader('Content-Type', 'text/html');
+ res.setHeader('Content-Length', Buffer.byteLength(html, 'utf8'));
+ res.end(html);
+}
+
+function tick(){
+ var now = new Date();
+ if(target_socket != null){
+  target_socket.send(now % 60);
+  console.log("send data in tick");
+ }
+}
+
+//setInterval(tick, 1000);
